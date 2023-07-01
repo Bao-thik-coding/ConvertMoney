af4d562ee6cf4f4e66f21be8d00d360295353be0
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-01 20:52:28 +0700

    Add project files.

---
+++ ConvertMoney/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float vnd;
+            Console.Write("Type in the VND to convert to USD: ");
+            vnd = float.Parse(Console.ReadLine());
+            float USD = vnd / 23000;
+            Console.WriteLine("Vnd convert to Usd = " + USD);
+            Console.ReadKey();
+        }
+    }
+}
